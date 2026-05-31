c875494105ffb7df8430e43da1457243508a6abf
mohamed <moja3222@gmail.com>
2026-05-31 05:18:44 +0300

    Refactor file handling in BaseController; update file paths and improve random string generation method; recreate .gitignore and .gitkeep files

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -8,9 +8,14 @@ class BaseController:
         self.app_settings=get_settings()
       
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
-        self.files_dir = os.path.join(self.base_dir, "assets", "files")
+        self.files_dir = os.path.join(self.base_dir, "assets/files")
         
-    def generate_random_string(self,legnth:int=12):
-        return " ".join(random.choices(string.ascii_lowercase+string.digits,k=legnth))
-        
-        
+    def generate_random_string(self, length: int = 12):
+        return "".join(
+            random.choices(
+                string.ascii_lowercase + string.digits,
+                k=length
+            )
+        )
+                
+            
