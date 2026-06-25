1fe1c3efe5137108c29305c735986925c28647b7
mohamed <moja3222@gmail.com>
2026-06-25 18:00:22 +0300

    Implement MongoDB connection management in FastAPI; add startup and shutdown event handlers

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-
 from routes import base,data
-
+from motor.motor_asyncio import AsyncIOMotorClient # type: ignore
+from helpers.config import get_settings
 
 
 app = FastAPI()
 
+async def startup_db_client():
+    settings = get_settings()
+    app.mongo_connection=AsyncIOMotorClient(settings.MONGO_URI)
+    app.db_client=app.mongo_connection[settings.MONGO_DB_NAME]
+    
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_connection.close()
+    
 app.include_router(base.base_router)
 app.include_router(data.data_router)
