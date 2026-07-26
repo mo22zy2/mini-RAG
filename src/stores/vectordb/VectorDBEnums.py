9997aa24ec321aa50f49d4b1629e9b259b833b4b
mohamed <moja3222@gmail.com>
2026-07-26 10:57:24 +0300

    Update requirements and extend VectorDBEnums: add pgvector and nltk to requirements, and define PgVectorTableSchemaEnums and PgVectorDistanceMethodEnum classes

--- src/stores/vectordb/VectorDBEnums.py
+++ src/stores/vectordb/VectorDBEnums.py
@@ -8,3 +8,16 @@ class DistanceMethodEnum(Enum):
     COSINE="cosine"
     DOT="dot"
     EUCLIDEAN="euclidean"
+    
+    
+class PgVectorTableSchemaEnums(Enum):
+    ID='id'
+    TEXT='text'
+    VECTOR='vector'
+    CHUNK_ID='chunk_id'
+    METADATA='metadata'
+    _PREFIX='pgvector'
+    
+class PgVectorDistanceMethodEnum(Enum):
+    COSINE="vector_cosine_ops"
+    DOT="vector_l2_ops"
